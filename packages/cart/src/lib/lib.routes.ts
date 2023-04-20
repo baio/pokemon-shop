bc18524492e56939ed56206d09f8dcaddb137821
baio <max.putilov@gmail.com>
2023-04-20 10:23:37 +0500

    cart

--- packages/cart/src/lib/lib.routes.ts
+++ packages/cart/src/lib/lib.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
-export const cartRoutes: Route[] = [];
+export const cartRoutes: Route[] = [
+  {
+    path: '',
+    component: CartComponent,
+  },
+];
--- packages/pokemon-shop/src/app/app.routes.ts
+++ packages/pokemon-shop/src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const appRoutes: Route[] = [
     path: 'pokemons',
     loadChildren: () => import('@tambo/pokemons').then((m) => m.PokemonsModule),
   },
+  {
+    path: 'cart',
+    loadChildren: () => import('@tambo/cart').then((m) => m.CartModule),
+  },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
 ];
