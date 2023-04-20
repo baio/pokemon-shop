6accd6cdc0903601aeb07fdea33245efbebb8b40
baio <max.putilov@gmail.com>
2023-04-20 12:45:56 +0500

    fix

--- packages/pokemons/src/index.ts
+++ packages/pokemons/src/index.ts
@@ -1,5 +1,3 @@
 export * from './lib/pokemons.module';
-export * from './lib/lib.routes';
-
-export * from './lib/lib.routes';
 export * from './lib/store/pokemons.selectors';
+export * from './lib/models';
---
+++ packages/pokemons/src/lib/models/index.ts
@@ -0,0 +1,3 @@
+export * from './pokemon.model';
+export * from './pokemons-list.model';
+export * from './pokemons-state.model';
