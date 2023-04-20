8d94cc93124890f31874457f82fb5aa9420fd5f9
baio <max.putilov@gmail.com>
2023-04-20 13:08:34 +0500

    cart summary

--- packages/pokemon-shop/src/app/app.component.ts
+++ packages/pokemon-shop/src/app/app.component.ts
@@ -3,35 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { PokemonName } from '@tambo/shared';
-import { selectCartAsList } from '@tambo/store/cart';
-import { PokemonsState, selectPokemons } from '@tambo/store/pokemons';
+import { CartSummary, selectCartSummary } from '@tambo/store/pokemons';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { Observable, combineLatest, map } from 'rxjs';
-export interface CartSummary {
-  count: number;
-  amount: number;
-}
-
-const reduceCartSummary = ([pokemons, cartList]: [
-  PokemonsState,
-  PokemonName[]
-]) =>
-  cartList.reduce(
-    (acc, v) => {
-      const pokemon = pokemons[v];
-      if (pokemon) {
-        return {
-          amount: acc.amount + (pokemon.price || 0),
-          count: acc.count + 1,
-        };
-      } else {
-        return { amount: acc.amount, count: acc.count + 1 };
-      }
-    },
-    { count: 0, amount: 0 }
-  );
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -44,9 +19,6 @@ const reduceCartSummary = ([pokemons, cartList]: [
 export class AppComponent {
   readonly cartSummary$: Observable<CartSummary>;
   constructor(store: Store) {
-    this.cartSummary$ = combineLatest([
-      store.select(selectPokemons),
-      store.select(selectCartAsList),
-    ]).pipe(map(reduceCartSummary));
+    this.cartSummary$ = store.select(selectCartSummary);
   }
 }
--- packages/store/pokemons/src/lib/pokemons.selectors.ts
+++ packages/store/pokemons/src/lib/pokemons.selectors.ts
@@ -1,4 +1,33 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { PokemonsState } from './models/pokemons-state.model';
+import { PokemonName } from '@tambo/shared';
+import { selectCartAsList } from '@tambo/store/cart';
 
 export const selectPokemons = createFeatureSelector<PokemonsState>('pokemons');
+
+export interface CartSummary {
+  count: number;
+  amount: number;
+}
+
+const reduceCartSummary = (pokemons: PokemonsState, cartList: PokemonName[]) =>
+  cartList.reduce(
+    (acc, v) => {
+      const pokemon = pokemons[v];
+      if (pokemon) {
+        return {
+          amount: acc.amount + (pokemon.price || 0),
+          count: acc.count + 1,
+        };
+      } else {
+        return { amount: acc.amount, count: acc.count + 1 };
+      }
+    },
+    { count: 0, amount: 0 }
+  );
+
+export const selectCartSummary = createSelector(
+  selectPokemons,
+  selectCartAsList,
+  reduceCartSummary
+);
